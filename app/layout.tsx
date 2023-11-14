5d6f122936584bc2d8ccb4c51bd62d3d60a96dba
KudaMukucha <kudamukucha80@gmail.com>
2023-11-14 11:06:59 +0200

    favicon test fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Todo App',
   description: 'This a todo app created using Next JS,Tailwind and DaisyUI.',
+  
 }
 
 export default function RootLayout({
